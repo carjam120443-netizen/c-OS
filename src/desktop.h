61654a394b874d323b4f4b3000af1ebee127fae3
carjam120443-netizen <carjam120443@gmail.com>
2026-08-31 15:11:36 -0400

    Expose native framebuffer desktop initialization

--- src/desktop.h
+++ src/desktop.h
@@ -6,7 +6,10 @@
 #define DESKTOP_HEIGHT 480
 
 void desktop_init(uint32_t *framebuffer, uint32_t width, uint32_t height);
+void desktop_init_framebuffer(uint32_t *framebuffer, uint32_t width, uint32_t height,
+                              uint32_t pitch_bytes, uint32_t bpp);
 void desktop_clear(uint32_t color);
 void desktop_draw(void);
+int desktop_available(void);
 
 #endif
