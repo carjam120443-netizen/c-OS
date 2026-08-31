03281476917eff38bb6433822833445144caf908
carjam120443-netizen <carjam120443@gmail.com>
2026-08-30 20:36:22 -0400

    Add c-OS pkg manager definitions

---
+++ user/pkg/pkg.h
@@ -0,0 +1,12 @@
+#ifndef COS_PKG_H
+#define COS_PKG_H
+
+#define PKG_DB_DIR "/var/db/pkg"
+#define PKG_REPO_FILE "/etc/pkg/repository"
+#define PKG_CACHE_DIR "/var/cache/pkg"
+#define PKG_MAX_NAME 128
+#define PKG_MAX_VERSION 64
+#define PKG_MAX_DESC 256
+#define PKG_MAX_PATH 512
+
+#endif
