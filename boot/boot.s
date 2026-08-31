bc55aacbb3b83e691ba68f5c118afddc8f06670d
carjam120443-netizen <carjam120443@gmail.com>
2026-08-31 15:11:07 -0400

    Request 640x480 framebuffer and pass Multiboot info to kernel

--- boot/boot.s
+++ boot/boot.s
@@ -1,6 +1,7 @@
 .set ALIGN,    1<<0
 .set MEMINFO,  1<<1
-.set FLAGS,    ALIGN | MEMINFO
+.set VIDEO,    1<<2
+.set FLAGS,    ALIGN | MEMINFO | VIDEO
 .set MAGIC,    0x1BADB002
 .set CHECKSUM, -(MAGIC + FLAGS)
 
@@ -9,12 +10,18 @@
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
+.long 0
+.long 640
+.long 480
+.long 32
 
 .section .text
 .global _start
 .type _start, @function
 _start:
     cli
+    push %ebx
+    push %eax
     call kmain
 
 .hang:
